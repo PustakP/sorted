499e3f4040c03ee76d1be383c8814a4e11248a38
Pustak Pathak <74170759+PustakP@users.noreply.github.com>
2024-06-27 19:44:27 +0530

    newfix

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="w-2/3 mx-auto">
+      <div className="w-5/6 mx-auto">
         <Orderblock1 />
        <Component />
        </div>
